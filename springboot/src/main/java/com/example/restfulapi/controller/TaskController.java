8bd53eb6c5e5377ea5e908a1ab7b7da77bfa8288
bigongja <dnrms47@naver.com>
2025-01-29 17:30:28 +0900

    Apply .gitignore

--- springboot/src/main/java/com/example/restfulapi/controller/TaskController.java
+++ springboot/src/main/java/com/example/restfulapi/controller/TaskController.java
@@ -1,5 +1,17 @@
 package com.example.restfulapi.controller;
 
+// java:HTTP 요청을 처리하는 Controller
+/** TaskController의 역할
+ *  @RestController, 
+ *  @RequestMapping, 
+ *  @GetMapping, 
+ *  @PostMapping, 
+ *  @PutMapping, 
+ *  @DeleteMapping 등
+ *  의 어노테이션을 사용하여 RESTful API 엔드포인트를 정의
+ * 
+ * -> 클라이언트의 HTTP 요청을 처리하고, Task 모델을 사용하여 데이터를 관리하고, HTTP 응답을 생성 
+ **/
 public class TaskController {
 
 }
